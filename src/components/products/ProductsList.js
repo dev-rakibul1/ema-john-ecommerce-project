3ee3483c931bd283464774c989a771021f031ed5
dev-rakibul1 <fl.rakibul@gmail.com>
2022-09-27 09:50:55 +0600

    project card calculation done

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredData } from "../../utilities/fakedb";
 import Card from "./card/Card";
 import Summary from "./summary/Summary";
 
@@ -6,16 +7,53 @@ const ProductsList = () => {
   const [products, setProducts] = useState([]);
   const [card, setCard] = useState([]);
 
+  // data load from API
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
+  // local storage data get as object
+  useEffect(() => {
+    const storedCard = getStoredData();
+    const saveCard = [];
+    for (let id in storedCard) {
+      const getId = products.find((product) => product.id === id);
+
+      if (getId) {
+        const quantity = storedCard[id];
+        getId.quantity = quantity;
+        saveCard.push(getId);
+        // console.log(quantity);
+      }
+    }
+    setCard(saveCard);
+  }, [products]);
+
+  // console.log(products);
+  // console.log(card);
+
   // add to button handler
-  const addToProductsCounter = (prod) => {
-    const newCard = [...card, prod];
+  const addToProductsCounter = (selectedProducts) => {
+    // console.log(selectedProducts);
+    let newCard = [];
+    const exists = products.find(
+      (product) => product.id === selectedProducts.id
+    );
+    if (!exists) {
+      selectedProducts.quantity = 1;
+      newCard = [...card, selectedProducts];
+    } else {
+      const rest = products.filter(
+        (product) => product.id !== selectedProducts.id
+      );
+      exists.quantity = exists.quantity + 1;
+      newCard = [...rest, exists];
+    }
+
     setCard(newCard);
+    addToDb(selectedProducts.id);
   };
 
   return (
--- src/components/products/summary/Summary.js
+++ src/components/products/summary/Summary.js
@@ -4,55 +4,63 @@ const Summary = (props) => {
   const len = props.card.length;
   const price = props.card;
 
-  // Total price
-  const totalPrice = price.reduce((prev, current) => prev + current.price, 0);
+  // product quantity
+  let quantity = 0;
+  let totalPrice = 0;
+  // let shipping = 0;
+  for (let product of price) {
+    quantity = quantity + product.quantity;
+    totalPrice = totalPrice + product.price * product.quantity;
+    // shipping = shipping + product.shipping;
+  }
 
   // Shipping charge
-  let shippingCharge = 0;
+  let shipping = 0;
   if (totalPrice === 0) {
-    shippingCharge = 0;
+    shipping = 0;
   } else if (totalPrice > 1000 && totalPrice < 2000) {
-    shippingCharge = 100;
+    shipping = 100;
   } else if (totalPrice > 2000 && totalPrice < 5000) {
-    shippingCharge = 200;
+    shipping = 200;
   } else if (totalPrice > 5000) {
-    shippingCharge = 500;
+    shipping = 500;
   } else {
-    shippingCharge = 10;
+    shipping = 10;
   }
 
   // tex amount
   // grandWithTax = TOTAL * ((100 + TAX PERCENTAGE) / 100);
-
   const tax = (totalPrice / 100) * 1.2;
   const totalTax = parseFloat(tax.toFixed(2));
+  // console.log(typeof totalTax);
 
   // Grand Total
-  const grandTotal = [totalPrice, totalTax, shippingCharge];
+  const grandTotal = [totalPrice, totalTax, shipping];
   const grandTotalPrice = grandTotal.reduce(
     (prev, current) => prev + current,
     0
   );
 
   return (
-    <div className="">
+    <div className="sticky top-0 p-2">
       <h4 className="text-center font-semibold text-xl mt-2 mb-5">
         Order Summary
       </h4>
       <p className="leading-8 flex justify-between">
-        <strong>Selected Items:</strong> <span>{len}</span>
+        <strong>Selected Items:</strong> <span>{quantity}</span>
       </p>
       <p className="leading-8 flex justify-between">
         <strong>Total price: </strong> <span>$ {totalPrice}</span>
       </p>
       <p className="leading-8 flex justify-between">
-        <strong>Shipping Charge: </strong> <span>$ {shippingCharge}</span>
+        <strong>Shipping Charge: </strong> <span>$ {shipping}</span>
       </p>
       <p className="leading-8 flex justify-between">
         <strong>Tax: </strong> <span>$ {totalTax}</span>
       </p>
       <p className="leading-8 flex justify-between">
-        <strong>Grand Total: </strong> <span></span>$ {grandTotalPrice}
+        <strong>Grand Total: </strong> <span></span>${" "}
+        {grandTotalPrice.toFixed(2)}
       </p>
 
       <button className="bg-sky-900 w-full py-2 px-4 mt-5 rounded text-white">
